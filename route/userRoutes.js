5e52061d06b2ae7ec4f9dba25b7127b9ae7f3643
Navneet2414 <navneetmpec@gmail.com>
2024-02-01 15:02:31 +0530

    correction in code

--- route/userRoutes.js
+++ route/userRoutes.js
@@ -2,7 +2,7 @@ const userController = require("../controller/userController");
 const express = require('express');
 const app = express();
 const router =express.Router();
-const Auth = require("../common/authentication");
+// const Auth = require("../common/authentication");
 
 router.post("/Register",userController.Register);
 router.post("/login",userController.Login);
